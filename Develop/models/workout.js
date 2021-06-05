24c843394df6da9787ea6853113b52ed16c412d1
ramondara10 <68656067+ramondara10@users.noreply.github.com>
2021-06-05 11:23:52 -0500

    creted files/folders and work on the models folder

---
+++ Develop/models/workout.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema(
+    {
+        day: {
+            type: Date,
+            default: () => new Date()
+        },
+        exercise: [
+            {
+                type: {
+                    type: String,
+                    trim: true,
+                    required: "Enter an exercise type"
+                },
+                name: {
+                    type: String,
+                    trim: true,
+                    required: "Enter an exercise name"
+                },
+                duration: {
+                    type: Number,
+                    required: "Enter an exercise duration in minutes"
+                },
+                weight: {
+                    type: Number
+                },
+                reps: {
+                    type: Number
+                },
+                sets: {
+                    type: Number
+                },
+                distance: {
+                    type: Number
+                }
+            }
+        ]
+    },
+    {
+        toJSON: {
+            //virtual properties when data is requested 
+            virtuals: true
+        }
+    }
+);
+//should make the shema dynamic
+workoutSchema.virtual('totalDuration').get(function () {
+    return this.exercises.reduce((total, exercise) => {
+        return total + exercise.duration;
+    }, 0);
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
